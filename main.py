df8b9773470389cc7f67b4e0c163e178be3810e1
KuGunmo <kgm9179@naver.com>
2021-08-08 15:51:23 +0900

    user, satelite class

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import time as t
+class user:
+    def __init__(self):
+        self.x=0
+        self.y=0
+class satelite:
+    def __init__(self):
+        self.x=0
+        self.y=0
+
